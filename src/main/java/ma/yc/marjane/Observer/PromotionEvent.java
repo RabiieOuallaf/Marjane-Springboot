0c8f910aeae4bf92b8bb3c020e45f3a9891ae7ea
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-11-29 11:02:38 +0100

    Promotion event

--- src/main/java/ma/yc/marjane/Observer/PromotionEvent.java
+++ src/main/java/ma/yc/marjane/Observer/PromotionEvent.java
@@ -1,4 +1,16 @@
 package ma.yc.marjane.Observer;
 
-public class PromotionEvent {
+import ma.yc.marjane.DTO.PromotionDTO;
+import org.springframework.context.ApplicationEvent;
+
+public class PromotionEvent extends ApplicationEvent {
+    private final PromotionDTO promotionDTO;
+
+    public PromotionEvent(Object source,PromotionDTO promotionDTO) {
+        super(source);
+        this.promotionDTO = promotionDTO;
+    }
+    public PromotionDTO getPromotionDTO() {
+        return promotionDTO;
+    }
 }
